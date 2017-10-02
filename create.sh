a2909607f090a79d585521a1da3641d81d5ebc3a
Lev Leninsky <logistikdesign@gmail.com>
2017-10-03 00:14:54 +0300

    Changed order for WorkingDirectory
    `eval sudo echo "WorkingDirectory=$SERVER_PATH" >> create.sh `

--- create.sh
+++ create.sh
@@ -10,11 +10,11 @@ cat <<SERVICE > "/lib/systemd/system/$name.service"
 Description=$3
 After=syslog.target
 
+[Install]
+WantedBy=multi-user.target
+
 [Service]
 Type=simple
 ExecStart=$command
 Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
 SERVICE
